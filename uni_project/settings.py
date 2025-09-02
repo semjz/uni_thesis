212652f85771ba735d26f8342a20692ce6a4ba1d
Saman Malakjan <sam.malakajan@gmail.com>
2025-09-02 16:35:30 +0330

    fix static file

--- uni_project/settings.py
+++ uni_project/settings.py
@@ -176,8 +176,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"; MEDIA_URL = "/media/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic output
+STATICFILES_DIRS = [BASE_DIR / "static"]  # only if this folder exists
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+MEDIA_URL = "/media/"
 MEDIA_ROOT  = BASE_DIR / "media"
 
 # Default primary key field type
